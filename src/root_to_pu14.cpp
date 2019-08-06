7f36700bdaafda5b9494f41bd12257399d2c30ca
ustcllh <usctllh@gmail.com>
2019-08-06 18:23:52 -0500

    for jewel 2.2.1 private

--- src/root_to_pu14.cpp
+++ src/root_to_pu14.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 #include "TFile.h"
@@ -23,6 +24,7 @@ int main(int argc, char *argv[])
    int NP;
    double Weight;
    double PID[MAX], PX[MAX], PY[MAX], PZ[MAX], PM[MAX], PStatus[MAX];
+   double H[13];
 
    Tree->SetBranchAddress("EWeight", &Weight);
    Tree->SetBranchAddress("NP", &NP);
@@ -32,6 +34,7 @@ int main(int argc, char *argv[])
    Tree->SetBranchAddress("PZ", &PZ);
    Tree->SetBranchAddress("PM", &PM);
    Tree->SetBranchAddress("PStatus", &PStatus);
+   Tree->SetBranchAddress("H", &H);
 
    int EntryCount = Tree->GetEntries();
    for(int iE = 0; iE < EntryCount; iE++)
@@ -40,14 +43,38 @@ int main(int argc, char *argv[])
 
       out << "# event " << iE << endl;
       out << "weight " << Weight << endl;
+
+      // H line in private jewel for partons
+      // double px1 = H[0]/100. * cos(H[2]/100.);
+      // double py1 = H[0]/100. * sin(H[2]/100.);
+      // double pz1 = H[0]/100. * sinh(H[8]/100.);
+      // double pm1 = H[1]/100.;
+      // int pid1 = H[6]==1? 21 : 0;	// is gluon
+      // int vtx1 = 1;
+      //
+      // double px2 = H[3]/100. * cos(H[5]/100.);
+      // double py2 = H[3]/100. * sin(H[5]/100.);
+      // double pz2 = H[3]/100. * sinh(H[12]/100.);
+      // double pm2 = H[4]/100.;
+      // int pid2 = H[7]==1? 21 : 0;	// is gluon
+      // int vtx2 = 1;
+      //
+      //
+      // out << px1 << " " << py1 << " " << pz1 << " " << pm1 << " " << pid1 << " " << vtx1 << endl;
+      // out << px2 << " " << py2 << " " << pz2 << " " << pm2 << " " << pid2 << " " << vtx2 << endl;
+
       for(int i = 0; i < NP; i++)
       {
-         if(PStatus[i] < 1.1 && PStatus[i] > 0.9)   // 1
+
+         if(PStatus[i] < 23.1 && PStatus[i] > 22.9)   // 23: parton
             out << PX[i] << " " << PY[i] << " " << PZ[i] << " " << PM[i] << " " << PID[i] << " " << 0 << endl;
-         if(PStatus[i] < 4.1 && PStatus[i] > 3.9)   // 4
+         if(PStatus[i] < 1.1 && PStatus[i] > 0.9)   // 1: final state particle
             out << PX[i] << " " << PY[i] << " " << PZ[i] << " " << PM[i] << " " << PID[i] << " " << 0 << endl;
-         if(PStatus[i] > 2.9 && PStatus[i] < 3.1)   // 3
+         if(PStatus[i] < 4.1 && PStatus[i] > 3.9)   // 4: incoming beam particle
+            out << PX[i] << " " << PY[i] << " " << PZ[i] << " " << PM[i] << " " << PID[i] << " " << 0 << endl;
+         if(PStatus[i] > 2.9 && PStatus[i] < 3.1)   // 3: documentation entry
             out << PX[i] << " " << PY[i] << " " << PZ[i] << " " << PM[i] << " " << PID[i] << " " << -1 << endl;
+
       }
       out << "end" << endl;
    }
@@ -57,7 +84,3 @@ int main(int argc, char *argv[])
 
    return 0;
 }
-
-
-
-
